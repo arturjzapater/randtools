9b3bbae82cff796d603dc6f3f67c5ff56059089d
SavagePixie <43502485+SavagePixie@users.noreply.github.com>
2020-01-05 16:22:12 +0100

    fixed bug where unifRange could return too many decimals

--- src/uniform.js
+++ src/uniform.js
@@ -6,7 +6,7 @@ const unifChoose = array => array[base(0, array.length - 1)]
 
 const unifRange = (from, to, decs=0) => {
     const mult = 10 ** getDecimals(decs, from, to)
-    return base(from * mult, to * mult) / mult
+    return Math.round(base(from * mult, to * mult)) / mult
 }
 
 module.exports = { unifChoose, unifRange}
