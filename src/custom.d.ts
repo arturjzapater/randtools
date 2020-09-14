e6a180de265c9079cfe885622342f4cd89609b34
arturjzapater <zapater.artur@gmail.com>
2020-09-14 14:25:14 +0200

    added custom.sequence

--- src/custom.d.ts
+++ src/custom.d.ts
@@ -1,3 +1,5 @@
 export as namespace custom
 
 export function choose(data: any[], chance: number[]): any
+
+export function sequence(start: number, chance: number[], opts?: { step?: number }): number
--- src/custom.js
+++ src/custom.js
@@ -1,10 +1,33 @@
+const getDecimals = require('./lib/decimalHandler')
 const { range } = require('./uniform')
 
+const isLte = a => b => a <= b
+
+const isFiniteOrThrow = (name, value) => {
+    if (!Number.isFinite(value)) {
+        throw new TypeError(`${name} should be a finite number. ${value} (${typeof value}) found.`)
+    }
+}
+
 const choose = (data, chance) => {
-    const elem = range(1, 100)
-    return data[chance.findIndex(x => elem <= x)]
+    const random = range(1, 100)
+    return data[chance.findIndex(isLte(random))]
+}
+
+const sequence = (start, chance, { step = 1 } = {}) => {
+    isFiniteOrThrow('start', start)
+    isFiniteOrThrow('step', step)
+
+    const decs = getDecimals(0, start, step)
+    const mult = 10 ** decs
+
+    const random = range(1, 100)
+    const index = chance.findIndex(isLte(random))
+
+    return ((start * mult) + (index * step * mult)) / mult
 }
 
 module.exports = {
     choose,
+    sequence,
 }
