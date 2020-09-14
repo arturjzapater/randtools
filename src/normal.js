7cfded55437908cbef42618f78b11b26d7466f59
arturjzapater <zapater.artur@gmail.com>
2020-09-14 13:47:36 +0200

    refactor

--- src/normal.js
+++ src/normal.js
@@ -3,23 +3,29 @@ const getDecimals = require('./lib/decimalHandler')
 const base = (m = 0, d = 1) => 
     (Math.sqrt(-2 * Math.log(Math.random())) * Math.cos(2 * Math.PI * Math.random())) * d + m
 
+const calculateStdDev = (from, to, devs) => (to - from) / (2 * devs)
+
+const isInBounds = (from, to, result) => result >= from && result <= to
+
+const isPositiveInt = num => typeof num === 'number' && num > 0
+
+const choose = (array, devs = 3) => array[range(0, array.length - 1, 0, devs)]
+
 const mean = (m, d, decs = 0) => {
     const mult = 10 ** decs
     return Math.round(base(m, d) * mult) / mult
 }
 
-const choose = (array, devs = 3) => array[range(0, array.length - 1, 0, devs)]
-
 const range = (from, to, decs = 0, devs = 3) => {
-    const deviations = devs < 1 || typeof devs != "number"
-        ? 3
-        : devs
+    const deviations = isPositiveInt(devs) ? devs : 3
+    const mean = (from + to) / 2
     const mult = 10 ** getDecimals(decs, from, to)
 
-    const result = Math.round(base((from+to)/2, (to-from)/(deviations*2)) * mult) / mult
-    return result < from || result > to
-        ? range(from, to, decs, devs)
-        : result
+    const result = Math.round(base(mean, calculateStdDev(to, from, deviations)) * mult) / mult
+
+    return isInBounds(from, to, result)
+        ? result
+        : range(from, to, decs, devs)
 }
 
 module.exports = {
